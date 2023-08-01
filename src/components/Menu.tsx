6d8ee778d9153b450f4139cc0e24293b52aa2049
marcos-c1 <marcos.b.campos14@gmail.com>
2023-08-01 15:54:17 -0300

    feat(frontend): login modal and fixing dropmenu from notes

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,21 +1,63 @@
 
 import {RxTextAlignJustify} from "react-icons/rx";
 import {MdDarkMode} from "react-icons/md";
+import { useState } from "react";
+
 const Menu = ({ isLogin }) => {
+    const [login, setLogin] = useState(false);
+    
+    function loginModal() {
+        showLoginMenu()
+        return (
+            <div className="container__login" id="login">
+                <input type="text" placeholder="Username"></input>
+                <input type="password" placeholder="Password"></input>
+                <small>Não tem cadastro?</small>
+                <ul className="login__link">
+                    <li>Google</li>
+                    <li>Github</li>
+                </ul>
+            </div>
+        )
+    }
+    
+    function handleHideBlur(e) {
+        let element = e.target;
+        let login = document.getElementById("login");
+        console.log(element);
+        if(element.style.display == "block"){
+            element.style.display = "none"
+        }
+        setLogin(!login);
+    }
+
+    function showLoginMenu() {
+        let element = document.getElementById("blur");
+        if(login) {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none"
+        }
+    }
+
   return (
     <div className="container__menu theme">
-        <ul>
-            <li className="icon"><RxTextAlignJustify size={23}/></li>
+        <div id="blur" style={{display: "none"}} onClick={handleHideBlur}>
+        </div>
+        <ul style={{alignItems: "center"}}>
+            <li className="icon"><RxTextAlignJustify size={20}/></li>
             <li>Notes</li>
             <div>
                 <ul className="container__menu__right">
                     <div className="backdrop__filter">
-                        <li className="icon icon__mode"><MdDarkMode size={23}/></li>
+                        <li className="icon icon__mode"><MdDarkMode size={20}/></li>
                     </div>
-                    <li>{ isLogin ? <img src="./logo.svg" alt="logo" /> : "Login"}</li>
+                    <li>{ isLogin ? <img src="./logo.svg" alt="logo" /> : <button onClick={() => setLogin(!login)} className="menu__login">Login</button>}</li>
                 </ul>
             </div>
-            
+            {
+                login ? loginModal() : null
+            }
         </ul>
     </div>
   )
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,20 +1,27 @@
 import { useEffect, useState } from "react";
-import { BsSearch } from "react-icons/bs";
+import { BsSearch, BsTrash, BsDownload } from "react-icons/bs";
 import { IoAddOutline } from "react-icons/io5";
 import { CiMenuKebab } from "react-icons/ci";
 import { FiTrash } from "react-icons/fi";
 import { HiPencil } from "react-icons/hi2";
+import { MdOutlineDriveFileRenameOutline } from "react-icons/md"; 
 
 
 const SideBar = () => {
     const allNotes = ["Note 1", "Note 2", "Note 3", "Note 4", "Note 5"];
     const [notes, setNotes] = useState(allNotes);
     
+    document.addEventListener('click', (e: Event) => {
+        let submenu = document.querySelector(".sidebar__menu");
+        if(!submenu?.contains(e.target) && e.target.id != "menu_icon" && !e.target.classList.contains("in") && !e.target.classList.contains("sidebar__context__menu")){
+            submenu?.classList.add("hidden");
+        }
+    });
+
     function handleSearch(e) {
         let value = e.target.value.toLowerCase().trim()
         if(value){
             let filter = allNotes.filter((note) => note.toLowerCase().includes(value))
-            console.log(filter)
             if(filter.length > 0)
                 setNotes(filter)
             else
@@ -44,6 +51,17 @@ const SideBar = () => {
         }
     }
 
+    function handleSubMenuIcon(e){
+        let submenu = document.querySelector(".sidebar__menu");
+        let sub = document.getElementById("submenu");
+        sub.style.objectPosition = `${e.clientX - 50}px ${e.clientY}px`;
+        if(submenu?.classList.contains("hidden")) {
+            submenu.classList.remove("hidden");
+        } else {
+            submenu.classList.add("hidden");
+        }
+    }
+
   return (
     <div className="container__sidebar theme">
         <div className="sidebar__searchbar">
@@ -56,20 +74,34 @@ const SideBar = () => {
                 <span className="icon icon__mode" id="addBtn" onClick={handleClickAddBtn}><IoAddOutline/></span>
             </div>
         </div>
-        <ul className="sidebar__notes">
-            {
-                notes.map((item: String, index: number) => {
-                    return (
-                        <div className="flex__row sidebar__note">
-                            <li aria-label={index.toString()} key={index} onClick={handleClickNote}>{item}</li>
-                            <div className="sidebar__notes__icon">
-                                <CiMenuKebab />
+            <ul className="sidebar__notes">
+                {
+                    notes.map((item: String, index: number) => {
+                        return (
+                            <div className="flex__row sidebar__note" 
+                            onMouseOver={(e) => e.target.classList.add("in")} 
+                            onMouseLeave={(e) => e.target.classList.remove("in")}
+                            aria-label={index.toString()}
+                            onClick={handleClickNote}>
+                                <li key={index}>{item}</li>
+                                <div  className="sidebar__notes__icon" onClick={handleSubMenuIcon} >
+                                    <CiMenuKebab id="menu_icon" />
+                                </div>
                             </div>
-                        </div>
-                    )
-                })
-            }
-        </ul>
+                        )
+                    })
+                }
+                <div className="sidebar__context__menu" >
+                                        <div className="sidebar__menu hidden" id="submenu">
+                                            <ul className="sidebar__menu__list">
+                                                <li className="sidebar__menu__item"><button className="sidebar__menu__button"><MdOutlineDriveFileRenameOutline fill={"gray"}/>Rename</button></li>
+                                                <li className="sidebar__menu__item"><button className="sidebar__menu__button sidebar__menu__button--delete"><BsTrash fill={"gray"}/>Delete</button></li>
+                                                <li className="sidebar__menu__item"><button className="sidebar__menu__button"><BsDownload fill={"gray"}/>Download</button></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+            </ul>
+            
     </div>
   )
 }
