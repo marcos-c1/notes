452ed09a77fe58c6836fbff7edac298278340276
marcos-c1 <marcos.b.campos14@gmail.com>
2023-08-11 17:28:48 -0300

    fix: handler Auth bug in compare password

--- backend/dist/controllers/authController.js
+++ backend/dist/controllers/authController.js
@@ -17,18 +17,25 @@ const handlerAuth = (req, res) => __awaiter(void 0, void 0, void 0, function* ()
     if (!username || !password) {
         return res.status(400).json({ 'message': 'Username and password are required' });
     }
-    const findUser = yield User.findOne({ username: username }).exec();
-    const match = bcrypt.compare(password, findUser.password);
-    if (match) {
-        const accessToken = jwt.sign({ "username": findUser.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' });
-        const refreshToken = jwt.sign({ "username": findUser.username }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
-        findUser.refreshToken = refreshToken;
-        res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
-        findUser.save().then((r) => console.log(`${r} salvo!`));
-        res.status(200).json({ 'id': findUser._id, 'accessToken': accessToken });
-    }
     else {
-        res.status(401);
+        try {
+            const findUser = yield User.findOne({ username: username }).exec();
+            const match = yield bcrypt.compare(password, findUser.password);
+            if (match) {
+                const accessToken = jwt.sign({ "username": findUser.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' });
+                const refreshToken = jwt.sign({ "username": findUser.username }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
+                findUser.refreshToken = refreshToken;
+                res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+                findUser.save().then((r) => console.log(`${r} salvo!`));
+                res.status(200).json({ 'id': findUser._id, 'accessToken': accessToken });
+            }
+            else {
+                res.status(500).json({ 'message': `Invalid password` });
+            }
+        }
+        catch (error) {
+            res.status(500).json({ 'message': `Server error ${error.message}` });
+        }
     }
 });
 module.exports = {
